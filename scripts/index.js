bcf06ff4f965cb8c0ffe2eb9edad7807cf7492d1
Nickolay.Vinokurov <nikolay.vinokurov@ics-it.ru>
2023-11-30 23:21:46 +0300

    Render cards

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const cardList = document.querySelector('.places__list');
+
 
 // @todo: Функция создания карточки
+function createCard(cardData, deleteCard) {
+    const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+
+    cardElement.querySelector('.card__title').textContent = cardData.name;
+    cardElement.querySelector('.card__image').src = cardData.link;
+
+    cardElement.querySelector('.card__delete-button').addEventListener('click', deleteCard);
+
+    return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard() {
+
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(cardData => cardList.append(createCard(cardData, deleteCard)));
