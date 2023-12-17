d93f2454fd4722e50f2bfe39911f14ff35d3cc5e
Nickolay.Vinokurov <nikolay.vinokurov@ics-it.ru>
2023-12-17 16:40:03 +0300

    Yay! Normal refactoring

--- src/index.js
+++ src/index.js
@@ -1,15 +1,115 @@
 import './pages/index.css';
-import { modalsData } from './scripts/modals.js';
 import { openModal, closeModal } from './scripts/modal.js'
 import { createCard, deleteCard } from './scripts/card.js';
 import { initialCards } from './scripts/cards.js';
 
+/*
+    В файле index.js описана инициализация приложения и основная логика страницы:
+        1. Поиск DOM-элементов на странице и навешивание на них обработчиков событий
+        2. Обработчики отправки форм
+        3. Функция-обработчик события открытия модального окна для редактирования профиля
+        4. Функция открытия модального окна изображения карточки
+    Также в index.js находится код, который отвечает за отображение шести карточек при открытии страницы.
+*/
+
 // DOM узлы
 const cardList = document.querySelector('.places__list');
+const editButton = document.querySelector('.profile__edit-button');
+const addButton = document.querySelector('.profile__add-button')
+const placesList = document.querySelector('.places__list');
+const editModal = document.querySelector('.popup_type_edit');
+const cardModal = document.querySelector('.popup_type_new-card');
+const imageModal = document.querySelector('.popup_type_image');
+const modals = [editModal, cardModal, imageModal];
+const editForm = document.forms['edit-profile'];
+const cardForm = document.forms['new-place'];
+
+// Навешивание обработчиков открытия модальных окон
+editButton.addEventListener('click', openProfileEditModal);
+placesList.addEventListener('click', openImageModal);
+addButton.addEventListener('click', function(evt) {
+    const addModal = document.querySelector('.popup_type_new-card');
+    openModal(addModal);
+});
+
+// Навешивание обработчиков закрытия модальных окон по нажатию на кнопку закрытия
+modals.forEach(modal => {
+    modal.addEventListener('click', function(evt) {
+        if (evt.target.classList.contains('popup__close')) {
+            closeModal(modal);
+        }
+    })
+})
+
+// Обработчик отправки формы редактирования профиля
+editForm.addEventListener('submit', function (evt) {
+    evt.preventDefault();
+        
+    const profileTitle = document.querySelector('.profile__title');
+    const profileDescription = document.querySelector('.profile__description');
+
+    profileTitle.textContent = editModal.querySelector('.popup__input_type_name').value;
+    profileDescription.textContent = editModal.querySelector('.popup__input_type_description').value;
+
+    closeModal(editModal);
+});
+
+// Обработчик отправки формы добавления новой карточки
+cardForm.addEventListener('sumbit', function (evt) {
+
+});
+
+// Функция-обработчик события открытия модального окна для редактирования профиля
+function openProfileEditModal(evt) {
+    const profileTitle = document.querySelector('.profile__title');
+    const profileDescription = document.querySelector('.profile__description');
+
+    editModal.querySelector('.popup__input_type_name').value = profileTitle.textContent;
+    editModal.querySelector('.popup__input_type_description').value = profileDescription.textContent;
+
+    openModal(editModal);
+}
+
+// Функция открытия модального окна изображения карточки
+function openImageModal(evt) {
+    if (!evt.target.classList.contains('card__image')) {
+        return;
+    }
+
+    const cardImage = evt.target;
+    const modalImageElement = imageModal.querySelector('.popup__image');
+    const modalCaprionElement = imageModal.querySelector('.popup__caption');
+
+    modalImageElement.src = cardImage.src;
+    modalCaprionElement.textContent = cardImage.parentElement.querySelector('.card__title').textContent;
+    
+    openModal(imageModal);
+}
 
 // Вывести карточки на страницу
 initialCards.forEach(cardData => cardList.append(createCard(cardData, deleteCard)));
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 // Обработчики событий для модального окна
 modalsData.forEach(function (modalData) {
     // Открытие модального окна при клике на кнопки
@@ -24,13 +124,8 @@ modalsData.forEach(function (modalData) {
             
             if (clickToCloseBtn || clickToOverlay) {
                 closeModal(modalData.modalObj);
-                removeKeydownHandler(modalData);
             }
         });
-
-        // Закрытие модального окна при нажатии на Escape
-        const keydownHandler = evt => closeModalByEscape(evt, modalData);
-        modalData.modalObj.closest('.page').addEventListener('keydown', keydownHandler);
 
         // Сохранение данных на форме модального окна
         let modalForm = modalData.modalObj.querySelector('.popup__form');
@@ -47,24 +142,7 @@ modalsData.forEach(function (modalData) {
 
                 // Закрыть модальное окно
                 closeModal(modalData.modalObj);
-                removeKeydownHandler(modalData);
             });
         }
-
-        // Сохранить обработчик для дальнейшего удаления
-        modalData.keydownHandler = keydownHandler;
     });
-});
-
-// Функция закрытия модального окна через Escape
-function closeModalByEscape(evt, modal) {
-    if (evt.key === 'Escape') {
-        closeModal(modal.modalObj);
-        removeKeydownHandler(modal);
-    }
-}
-
-// Функция удаление обработчика нажатия клавиши
-function removeKeydownHandler(modal) {
-    modal.modalObj.closest('.page').removeEventListener('keydown', modal.keydownHandler);
-}
+});*/
--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -1,56 +1,46 @@
+/*
+    В файле modal.js описаны функции для работы с модальными окнами:
+        1. Функция открытия модального окна
+        2. Функция закрытия модального окна
+        3. Функция-обработчик события нажатия Esc
+        4. Функция-обработчик события клика по оверлею
+*/
+
 // Функция открытия модального окна
-export function openModal(modalData) {
-    switch (modalData.modalType) {
-        case 'profile':
-            openProfileModal(modalData);
-            break;
-
-        case 'newCard':
-            openNewCardModal(modalData);
-            break;
-
-        case 'image':
-            openImageModal(modalData)
-            break;
-    }
+export function openModal(modal) {
+    modal.classList.add('popup_is-opened');
+    document.addEventListener('keydown', closeModalByEsc);
+    modal.addEventListener('click', closeModalByOverlayClick);
 }
 
 // Функция закрытия модального окна
 export function closeModal(modal) {
     modal.classList.remove('popup_is-opened');
+    document.removeEventListener('keydown', closeModalByEsc);
 }
 
-// Открытие модального окна профиля
-function openProfileModal(modalData) {
-    let profileTitle = document.querySelector('.profile__title');
-    let profileDescription = document.querySelector('.profile__description');
-    modalData.modalObj.querySelector('.popup__input_type_name').value = profileTitle.textContent;
-    modalData.modalObj.querySelector('.popup__input_type_description').value = profileDescription.textContent;
+// Функция-обработчик события нажатия Esc
+function closeModalByEsc(evt) {
+    if (evt.key === 'Escape') {
+        let modal = getOpenedModal();
+        closeModal(modal);
+    }
+}
 
-    modalData.modalObj.classList.add('popup_is-opened');
+// Функция-обработчик события клика по оверлею
+function closeModalByOverlayClick(evt) {        
+    if (evt.target.classList.contains('popup')) {
+        let modal = getOpenedModal();
+        closeModal(modal);
+    }
+}
+
+// Функция получения открытого модального окна
+function getOpenedModal() {
+    return document.querySelector('.popup_is-opened');
 }
 
 // Функция открытия модального окна новой карточки
 function openNewCardModal(modalData) {
-    modalData.modalObj.classList.add('popup_is-opened');
-}
-
-// Функция добавления изображение в модальное окно
-function openImageModal(modalData) {
-    // Проверить наличие изображения
-    let cardImage = modalData.targetObj.querySelector('.card__image');
-    if (!cardImage){
-        return;
-    }
-
-    // Установить изображение в модальном окне
-    let imageElement = modalData.modalObj.querySelector('.popup__image');
-    imageElement.src = cardImage.src;
-
-    // Установить описание в модальном окне
-    let popupCaption = modalData.modalObj.querySelector('.popup__caption');
-    popupCaption.textContent = cardImage.parentElement.querySelector('.card__title').textContent;
-
-    // Отобразить модальное окно
     modalData.modalObj.classList.add('popup_is-opened');
 }
